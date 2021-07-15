a3a9f411533b3886e0b74a58f98f0a50d62e9895
afiffahreza <afiffahreza3@gmail.com>
2021-07-15 17:58:38 +0700

    Add routes for toko API

--- main.go
+++ main.go
@@ -1,22 +1,38 @@
 package main
 
 import (
-	"net/http"
-
+	"github.com/afiffahreza/dorayaki-backend/connection"
+	"github.com/afiffahreza/dorayaki-backend/controller"
+	"github.com/afiffahreza/dorayaki-backend/repository"
+	"github.com/afiffahreza/dorayaki-backend/service"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+var (
+	db             *gorm.DB                  = connection.SetupDatabaseConnection()
+	tokoRepository repository.TokoRepository = repository.NewTokoRepository(db)
+	tokoService    service.TokoService       = service.NewTokoService(tokoRepository)
+	tokoController controller.TokoController = controller.NewTokoController(tokoService)
 )
 
 func main() {
+	defer connection.CloseDatabaseConnection(db)
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"data": "Hello, World!"})
-	})
+	/*
+		r.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"data": "Hello, World!"})
+		})
+	*/
 
-	r.GET("/haha/:id", func(c *gin.Context) {
-		var a = c.Param("id")
-		c.JSON(http.StatusOK, gin.H{"data": a})
-	})
+	tokoRoutes := r.Group("api/toko")
+	{
+		tokoRoutes.GET("/", tokoController.All)
+		tokoRoutes.POST("/", tokoController.Insert)
+		tokoRoutes.PUT("/:id", tokoController.Update)
+		tokoRoutes.DELETE("/:id", tokoController.Delete)
+	}
 
 	r.Run()
 }
